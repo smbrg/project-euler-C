1a5ca833d56acba6f71c4b0bc57a8d2cee1c144a
smbrg <smbrg17@gmail.com>
2017-05-29 22:21:30 -0700

    finished problem 2, edited prob 1

--- problem1/main.c
+++ problem1/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     int ans;
     int i;
     for (i = 0; i < 1000; i++) {
---
+++ problem2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MAX 4000000
+
+int main(int argc, char *argv[]) {
+    long temp;
+    long cur = 1;
+    long prev = 0;
+    long result = 0;
+    
+    while (cur < MAX) {
+        if (cur%2 == 0) {
+            result += cur; 
+        }
+        temp = cur;
+        cur += prev;
+        prev = temp;
+    }
+
+    fprintf(stdout, "%ld\n", result);
+
+    return 0;
+}
