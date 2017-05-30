5bea28260bc78ef987bf6b9e3c94a6e92fd2945a
smbrg <smbrg17@gmail.com>
2017-05-29 22:27:16 -0700

    done with prob3

---
+++ problem3/main.c
@@ -0,0 +1,25 @@
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?*/
+
+#include <stdio.h>
+
+#define QUERY_NUM 600851475143
+
+int main(int argc, char *argv[]) {
+    long remaining = QUERY_NUM;
+    long ans = 2;
+
+    while (remaining%ans == 0) {
+        remaining /= ans;
+    }
+
+    ans -= 1;
+    while (remaining > 1) {
+        ans += 2;
+        while (remaining%ans == 0) {
+            remaining /= ans;
+        }
+    }
+    fprintf(stdout, "%ld\n", ans);
+}
